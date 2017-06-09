76507e354d269e1630d0789677804229ff1d1ae1
Hernan Y.Ke <yuriqiao@gmail.com>
2017-06-09 12:22:35 -0400

    4-1

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
+var bodyParser=require('body-parser');
 
 var messages = [{text:'a',owner:'ke'},{text:'other',owner:'ma'}]
-
+app.use(bodyParser.json());
 app.use((req,res,next)=>{
     res.header("Access-Control-Allow-Origin","*");
     res.header("Access-Control-Allow-Headers","Origin,X-Requested-With,Content-Type,Accept");
@@ -18,5 +19,10 @@ app.get('/messages',(req,res)=>{
     res.json(messages)
 })
 
+app.post('/message',(req,res)=>{
+    //res.send('1')
+    console.log(req.body);
+    res.sendStatus(200);
+})
 
 app.listen(1234);
