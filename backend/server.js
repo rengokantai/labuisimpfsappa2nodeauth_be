f4c1e30a72fef01e878fd669f3ca4f157d984b76
Hernan Y.Ke <yuriqiao@gmail.com>
2017-06-08 23:51:06 -0400

    3-2

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
 
+var messages = [{text:'a',owner:'ke'},{text:'other',owner:'ma'}]
+
 app.get('/',(req,res)=>{
     res.send('1')
+    //res.json(messages)
+})
+app.get('/messages',(req,res)=>{
+    //res.send('1')
+    res.json(messages)
 })
 
-app.listen(1234)
+app.listen(1234);
